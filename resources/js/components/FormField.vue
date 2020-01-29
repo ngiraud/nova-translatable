34a3d157acb18551900db9c56f30e30480a5b46a
Nicolas Giraud <contact@ngiraud.me>
2020-01-29 18:14:10 +0100

    Key value handle empty object (#3)
    * Format file
    
    * handle the empty object for keyvalue

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -24,61 +24,71 @@
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova';
-import TranslatableField from '../mixins/TranslatableField';
-import LocaleTabs from './LocaleTabs';
+  import {FormField, HandlesValidationErrors} from 'laravel-nova';
+  import TranslatableField from '../mixins/TranslatableField';
+  import LocaleTabs from './LocaleTabs';
 
-export default {
-  components: { LocaleTabs },
-  mixins: [HandlesValidationErrors, FormField, TranslatableField],
-  props: ['field', 'resourceId', 'resourceName'],
+  export default {
+    components: {LocaleTabs},
+    mixins: [HandlesValidationErrors, FormField, TranslatableField],
+    props: ['field', 'resourceId', 'resourceName'],
 
-  methods: {
-    setInitialValue() {
-      // Do nothing
-    },
+    methods: {
+      setInitialValue() {
+        // Do nothing
+      },
 
-    fill(formData) {
-      // Add value to FormData
-      for (const locale of this.locales) {
-        formData.append(
-          `${this.field.attribute}[${locale.key}]`,
-          this.fieldValueMustBeAnObject ? JSON.stringify(this.value[locale.key]) : this.value[locale.key]
-        );
-      }
-    },
+      fill(formData) {
+        // Add value to FormData
+        for (const locale of this.locales) {
+          let value = this.value[locale.key]
 
-    getComponentField(locale) {
-      return {
-        ...this.field,
-        value: this.value[locale.key] || '',
-        attribute: `${this.field.attribute}.${locale.key}`,
-      }
-    }
-  },
-  computed: {
-    errorAttributes() {
-      const errorAttributes = {};
-      for (const locale of this.locales) {
-        errorAttributes[locale.key] = `${this.field.attribute}.${locale.key}`;
-      }
-      return errorAttributes;
-    },
-  },
-
-  created() {
-    this.$nextTick(() => {
-      for (const locale of this.locales) {
-        const attributeToWatch = this.fieldValueMustBeAnObject ? 'finalPayload' : 'value'
-
-        this.$watch(
-          `$refs.${locale.key}Component.0.${attributeToWatch}`,
-          (newValue) => {
-            this.value[locale.key] = newValue
+          if (this.fieldValueMustBeAnObject) {
+            if (Object.keys(value).length > 0) {
+              value = JSON.stringify(this.value[locale.key])
+            } else {
+              value = ''
+            }
           }
-        );
+
+          formData.append(
+            `${this.field.attribute}[${locale.key}]`,
+            value
+          );
+        }
+      },
+
+      getComponentField(locale) {
+        return {
+          ...this.field,
+          value: this.value[locale.key] || '',
+          attribute: `${this.field.attribute}.${locale.key}`,
+        }
       }
-    })
-  }
-};
+    },
+    computed: {
+      errorAttributes() {
+        const errorAttributes = {};
+        for (const locale of this.locales) {
+          errorAttributes[locale.key] = `${this.field.attribute}.${locale.key}`;
+        }
+        return errorAttributes;
+      },
+    },
+
+    created() {
+      this.$nextTick(() => {
+        for (const locale of this.locales) {
+          const attributeToWatch = this.fieldValueMustBeAnObject ? 'finalPayload' : 'value'
+
+          this.$watch(
+            `$refs.${locale.key}Component.0.${attributeToWatch}`,
+            (newValue) => {
+              this.value[locale.key] = newValue
+            }
+          );
+        }
+      })
+    }
+  };
 </script>
